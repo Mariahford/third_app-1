7fa71c19285b8775d91a6eabcf9a4d52fe3844cc
Mariah Ford <mford@chicagotechacademy.org>
2014-04-16 14:42:56 -0500

    finished layout routes and associated test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,41 +1,48 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  subject { pages}
+
   describe "Home page" do
-    it "Should have the content Sample App" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      page.should have_selector("h1", :text =>'Sample App')
+    before {visit root_path}
+      
+    it { should have_selector("h1", text: 'Sample App') }    
+    it {should have_selector("title", text: 'Home Page') }
     end
-    it "Should have a title" do
-      visit '/static_pages/home'
-      page.should have_selector("title", :text =>'Home Page')
-    end
-  end
   describe "Help page" do
-      it "Should have the content Helpe App Page" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/help'
-      page.should have_selector("h1", :text => 'Help Page')
+    before  {visit help_path}
+
+    it { should have_selector("h1", text: 'Help Page ') }    
+    it {should have_selector("title", text: 'Help Page') }
     end
-      it "Should have a title" do
-      visit '/static_pages/help'
-      page.should have_selector("title", :text =>'Help Page')
-    end
-     it "Should have a title" do
-      visit '/static_pages/home'
-      page.should have_selector("title", :text =>'Home Page')
-    end
-  end
   describe "About Us page" do
-      it "Should have the content About Us" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/about'
-      page.should have_selector("h1", :text => 'About Us')
+    before  {visit about_path}
+
+    it { should have_selector("h1", text: 'About Us') }    
+    it {should have_selector("title", text: 'About Page') }
     end
-      it "Should have a title" do
-      visit '/static_pages/about'
-      page.should have_selector("title", :text =>'About Page')
+   describe "Contact page" do
+    before  {visit contact_path}
+
+      it { should have_selector("h1", text: 'Contact Us') }    
+    it {should have_selector("title", text: 'Contact Page') }
     end
-  end
+    it "should have the correct links" do 
+    visit root_path
+    click_link "About"
+    page.should have_selector 'tilte', title: "About us"
+    click_link "Help"
+    page.should have_selector 'tilte', title: "Help"
+    click_link "Contact"
+    page.should have_selector 'tilte', title: "Contact"
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'tilte', title: "Sign up"
+
 end
+
+
+
+Ruby1.9.3/lib/ruby/gems/1.9.1/gems/actionpack-3.2.13/lib/action_dispatch/routing/mapper.rb:179:in `default_controller_and_action': missing :action (ArgumentError)
+
